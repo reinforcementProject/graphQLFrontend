b4ff9dd2530cb3b5e963ec7a828db9df7fd672f2
manjeet <manjeet175@gmail.com>
2018-09-05 13:09:49 -0400

    setup the environment by installing the necessary packages and configuring  the package.json file, .babelrc, .gitignore, webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('fs');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: __dirname + '/client/index.js',
+	output:{
+		path : __dirname + '/build',
+		filename:'webpack-bundle.js'
+	},
+	module:{
+		rules:[
+			{
+				test:/\.js$/,
+				exclude:/node_modules/,
+				loader:['babel-loader']
+			}
+		]
+	},
+	devServer:{
+		port:3000,
+		contentBase: './client',
+		// proxy: {
+    //   "/auth": {
+		// 		changeOrigin: true,
+		// 		target:"http://127.0.0.1:8080",
+		// 	}
+		// }
+	},
+		plugins:[
+			new CleanWebpackPlugin('build'),
+			new HtmlWebpackPlugin({
+				title:'Movies',
+				filename:'index.html',
+				template:'./index.html'
+			})
+		]
+}
