89a6f39f922cf9a66916a177603be1b8ba89ee14
leccih <ses2tst@gmail.com>
2018-09-05 21:23:30 -0400

    add schema and url creation, API working

--- schema/schema.js
+++ schema/schema.js
@@ -1,18 +1,38 @@
-const graphql = require('graphql');
-const Movie = require('../model/movieModel');
-
 const {
-  GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLInt, GraphQLList,
-} = graphql;
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLList,
+  GraphQLFloat
+} = require('graphql');
+const Movie = require('../model/movieModel');
+const fetch = require('node-fetch');
+
+const baseURL =
+  'https://api.themoviedb.org/3/discover/movie?api_key=5d576382955ff5829fc3844390db4427';
+
+// this function builds the URL based on user's selection
+function buildURL(baseURL, input) {
+  if (input.year) {
+    let yearurl = baseURL + '&primary_release_year=' + input.year;
+    return yearurl;
+  } else {
+    let nameurl =
+      'https://api.themoviedb.org/3/search/movie?api_key=5d576382955ff5829fc3844390db4427&language=en-US&query=' +
+      input.name;
+    return nameurl;
+  }
+}
 
 const MovieType = new GraphQLObjectType({
   name: 'Movie',
   fields: () => ({
     name: { type: GraphQLString },
     year: { type: GraphQLInt },
-    overview: { type: GraphQLString },
-    rating: { type: GraphQLInt },
-  }),
+    rating: { type: GraphQLFloat },
+    poster: { type: GraphQLString }
+  })
 });
 
 const RootQuery = new GraphQLObjectType({
@@ -20,21 +40,34 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     movies: {
       type: new GraphQLList(MovieType),
-      args: { 
-              year: { type: GraphQLInt },
-              name: { type: GraphQLString }
-            },
-      resolve(parents, args) {
-        if(args.year){
-          return Movie.find({year: args.year})
-        } else if (args.name){
-          return Movie.find({name: args.name})
-        } else {
-          return Movie.find({})
-        }
+      args: {
+        year: { type: GraphQLInt },
+        name: { type: GraphQLString }
       },
-    },
-  },
+      resolve(parents, args) {
+        console.log(args);
+        const data = fetch(buildURL(baseURL, args))
+          .then(response => {
+            if (!response.ok) {
+              throw error(response.statusText);
+            }
+            return response.json();
+          })
+          .then(response =>
+            response.results.map(movie => {
+              let newMovie = {
+                name: movie.title,
+                year: Number(movie.release_date.substring(0, 4)),
+                rating: movie.vote_average,
+                poster: movie.poster_path
+              };
+              return newMovie;
+            })
+          );
+        return data;
+      }
+    }
+  }
 });
 
 const Mutation = new GraphQLObjectType({
@@ -46,7 +79,7 @@ const Mutation = new GraphQLObjectType({
         name: { type: GraphQLString },
         year: { type: GraphQLInt },
         overview: { type: GraphQLString },
-        rating: { type: GraphQLInt },
+        rating: { type: GraphQLInt }
       },
       resolve(parents, args) {
         const movie = new Movie({
@@ -56,12 +89,12 @@ const Mutation = new GraphQLObjectType({
           rating: args.rating
         });
         return movie.save();
-      },
-    },
-  },
+      }
+    }
+  }
 });
 
 module.exports = new GraphQLSchema({
   query: RootQuery,
-  mutation: Mutation,
+  mutation: Mutation
 });
